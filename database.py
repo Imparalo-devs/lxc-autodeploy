f302cb6328c4740c4784eaedb1c9fb20c34b34db
Imparalo-devs <sviluppo-interno@happywpress.com>
2026-03-27 13:49:15 +0100

    feat: parametrize YOUR_NODE [vibe-code]

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ def fetch_lxc_parameters():
     db_path = os.path.join(os.getcwd(), 'lxc_deploy.db')
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    cursor.execute('SELECT memory, network, disks, containername FROM `lxc to deploy`')
+    cursor.execute('SELECT memory, network, disks, containername, node FROM `lxc to deploy`')
     rows = cursor.fetchall()
     conn.close()
-    return [{'memory': row[0], 'network': row[1], 'disks': row[2], 'containername': row[3]} for row in rows]
+    return [{'memory': row[0], 'network': row[1], 'disks': row[2], 'containername': row[3], 'node': row[4]} for row in rows]
